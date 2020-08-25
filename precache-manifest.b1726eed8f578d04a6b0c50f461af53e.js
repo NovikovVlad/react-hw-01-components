ae20362a6f55c91b6bc7db4cc1cce06ab7a193af
Vlad <nv.novikovvlad@gmail.com>
2020-08-25 18:25:24 +0300

    Updates

---
+++ precache-manifest.b1726eed8f578d04a6b0c50f461af53e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c7c0ba9bc9206744d3742943f615caf",
+    "url": "/react-hw-01-components/index.html"
+  },
+  {
+    "revision": "65552bac633604fce768",
+    "url": "/react-hw-01-components/static/css/main.fd0a79e3.chunk.css"
+  },
+  {
+    "revision": "ef0bc5b2598f7f3c1161",
+    "url": "/react-hw-01-components/static/js/2.55645800.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-hw-01-components/static/js/2.55645800.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65552bac633604fce768",
+    "url": "/react-hw-01-components/static/js/main.16f6ee45.chunk.js"
+  },
+  {
+    "revision": "6fe3cc52a8b0257f05d9",
+    "url": "/react-hw-01-components/static/js/runtime-main.459b7fff.js"
+  }
+]);
